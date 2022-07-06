44a9d13064067d1f4981e660df22ec910187cf20
Reginald Wong <reginald_wong@outlook.com>
2022-07-06 09:45:01 -0400

    add password protection

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const File = require('./models/File');
 
 const express = require('express');
 const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
 
 // initialize multer library where all file uploads go into 'uploads' folder
 const upload = multer({ dest: 'uploads' });
@@ -33,13 +35,26 @@ app.post('/upload', upload.single('file'), async (req, res) => {
   res.render('index', { fileLink: `${req.headers.origin}/file/${file.id}` });
 });
 
-app.get('/file/:id', async (req, res) => {
+app.route('/file/:id').get(handleDownload).post(handleDownload);
+
+async function handleDownload(req, res) {
   const file = await File.findById(req.params.id);
+
+  if (file.password != null) {
+    if (req.body.password == null) {
+      res.render('password');
+      return;
+    }
+    if (!(await bcrypt.compare(req.body.password, file.password))) {
+      res.render('password', { error: true });
+      return;
+    }
+  }
   file.downloadCount++;
   await file.save();
   console.log(file.downloadCount);
   res.download(file.path, file.originalName);
-});
+}
 
 const port = process.env.PORT || 3000;
 
