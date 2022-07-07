3fcb12e85369c2f515c7a0ad477d6bb8819d5da5
Reginald Wong <reginald_wong@outlook.com>
2022-07-07 19:58:20 -0400

    add form styling

--- server.js
+++ server.js
@@ -5,17 +5,18 @@ const multer = require('multer');
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const File = require('./models/File');
+const path = require('path');
 
 const express = require('express');
 const app = express();
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(__dirname + '/public'));
 
 // initialize multer library where all file uploads go into 'uploads' folder
 const upload = multer({ dest: 'uploads' });
 
 mongoose.connect(process.env.DB_URL);
 
+app.set('views', path.join(__dirname, 'views'));
 // SPECIFY DEFAULT VIEW ENGINE
 app.set('view engine', 'ejs');
 
